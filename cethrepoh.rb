cd0ab76e52f6e2fc84cfee925c4ef56aa07e6106
alexdforeman <alexdforeman@gmail.com>
2013-08-12 16:41:21 +0100

    OO'ed it for second pass. Added flat file, ios and BB10 classes

--- cethrepoh.rb
+++ cethrepoh.rb
@@ -23,39 +23,93 @@ raise OptionParser::MissingArgument if $DICTIONARY_FILE.nil?
 raise OptionParser::MissingArgument if $FILE.nil?
 raise OptionParser::MissingArgument if $TYPE.nil?
 
-###
-# Methods
-def uri?(string)
-  uri = URI.parse(string)
-  %w( http https ).include?(uri.scheme)
-rescue URI::BadURIError
-  false
-rescue URI::InvalidURIError
-  false
+class Cethrepoh
+  def initialize(dict, whitelist)
+    @dictionary = Set.new
+    for word in File.open(dict, 'r') {|file| file.read }.downcase.split(/\r?\n/)
+      @dictionary.add(word)
+    end
+    if File.file?(whitelist)
+      for word in File.open(whitelist, 'r') {|file| file.read }.downcase.split(/\r?\n/)
+        @dictionary.add(word)
+      end
+    end
+
+  end
+
+  def spellcheck(file)
+    for word in File.open(file, 'r') { |word| word.read }.downcase.split(/\r?\n/)
+      stripword = word.gsub(/[…\\\?\:\!\.\,\;\(\)0-9©\+-]/, '').downcase.strip
+      violations.add(stripword) unless uri?(word) || @dictionary.member?(word) || @dictionary.member?(stripword)
+    end
+    return violations
+  end
+
+  def uri?(string)
+    uri = URI.parse(string)
+    %w( http https ).include?(uri.scheme)
+  rescue URI::BadURIError
+    false
+  rescue URI::InvalidURIError
+    false
+  end
 end
 
-###
-# Program
-if File.file?($WHITELIST_FILE)
-  WHITELIST = File.open($WHITELIST_FILE, 'r') {|file| file.read }.downcase.split(/\r?\n/)
-else
-  WHITELIST = []
+class AndroidSC < Cethrepoh
+  def spellcheck(file)
+    violations = Set.new
+    doc = Nokogiri::HTML(open($FILE))
+    doc.xpath('//resources/string').each do |link|
+      for word in link.content.downcase.split()
+        stripword = word.gsub(/[…\\\?\:\!\.\,\;\(\)0-9©\+-]/, '').downcase.strip
+        violations.add(stripword) unless uri?(word) || @dictionary.member?(word) || @dictionary.member?(stripword)
+      end
+    end
+    return violations
+  end
 end
-DICTIONARY_WORDS = File.open($DICTIONARY_FILE, 'r') {|file| file.read }.downcase.split(/\r?\n/)
-#FILE_WORDS = Set.new
-VIOLATIONS = Set.new
 
+class BlackBerrySC < Cethrepoh
+  def spellcheck(file)
+    violations = Set.new
+    doc = Nokogiri::HTML(open($FILE))
+    doc.xpath('//context/message/source').each do |link|
+      for word in link.content.downcase.split()
+        stripword = word.gsub(/[…\\\?\:\!\.\,\;\(\)0-9©\+-]/, '').downcase.strip
+        violations.add(stripword) unless uri?(word) || @dictionary.member?(word) || @dictionary.member?(stripword)
+      end
+    end
+    return violations
+  end
+end
 
-
-doc = Nokogiri::HTML(open($FILE))
-doc.xpath('//resources/string').each do |link|
-  for word in link.content.downcase.split()
-    stripword = word.gsub(/[…\\\?\:\!\.\,\;\(\)0-9©\+-]/, '').downcase.strip
-    VIOLATIONS.add(stripword) unless uri?(word) || WHITELIST.index(word) || WHITELIST.index(stripword) || DICTIONARY_WORDS.include?(stripword)
+class IosSC < Cethrepoh
+  def spellcheck(file)
+    violations = Set.new
+    for line in File.open(file, 'r') { |line| line.read }.downcase.split(/\r?\n/)
+      i = line.index("=")
+      if i
+        words = line[i + 1..-1].gsub('"', '').split
+        for word in words
+          stripword = word.gsub(/[…\\\?\:\!\.\,\;\(\)0-9©\+-]/, '').downcase.strip
+          violations.add(stripword) unless uri?(word) || @dictionary.member?(word) || @dictionary.member?(stripword)
+        end
+      end
+    end
+    return violations
   end
 end
 
 
+if $TYPE == 'android'
+  spellchecker = AndroidSC.new $DICTIONARY_FILE, $WHITELIST_FILE
+elsif $TYPE == 'bb10'
+  spellchecker = BlackBerrySC.new $DICTIONARY_FILE, $WHITELIST_FILE
+elsif $TYPE == 'ios'
+  spellchecker = IosSC.new $DICTIONARY_FILE, $WHITELIST_FILE
+end
+
+VIOLATIONS = spellchecker.spellcheck($FILE)
 if VIOLATIONS.size() > 0
   puts "You have " + VIOLATIONS.size().to_s + "Violations"
   for word in VIOLATIONS
@@ -63,5 +117,4 @@ if VIOLATIONS.size() > 0
   end
   abort
 end
-
 puts "No Violations Detected"
